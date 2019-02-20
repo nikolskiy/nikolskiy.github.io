7e731729d3c08af8084adc867815c79e23b7e901
Denis Nikolskiy <fsdvn1@gmail.com>
2019-02-21 01:48:10 +0500

    First version of the landing page was added.

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -19,9 +19,9 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Denis'
-copyright = '2019, Denis'
-author = 'Denis'
+project = 'Denis Nikolskiy'
+copyright = '2019, Denis Nikolskiy'
+author = 'Denis Nikolskiy'
 
 # The short X.Y version
 version = ''
@@ -80,13 +80,15 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'prev_next_buttons_location': 'top',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
---
+++ sphinx/my/side-projects.rst
@@ -0,0 +1,4 @@
+Side Projects
+=============
+* `Poloniex API client <https://github.com/nikolskiy/poloniex>`_
+* Experiment with `GRPC mutual TLS auth <https://github.com/nikolskiy/python-grpc-mutual-tls-auth>`_
